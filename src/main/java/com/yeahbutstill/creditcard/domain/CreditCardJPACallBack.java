26ccc7ee309a7727fb72402061d0fed7cd6202da
yeahbutstill <mayadani28@gmail.com>
2023-03-23 00:13:11 +0700

    implement callback encryption

--- src/main/java/com/yeahbutstill/creditcard/domain/CreditCardJPACallBack.java
+++ src/main/java/com/yeahbutstill/creditcard/domain/CreditCardJPACallBack.java
@@ -1,5 +1,7 @@
 package com.yeahbutstill.creditcard.domain;
 
+import com.yeahbutstill.creditcard.config.SpringContextHelper;
+import com.yeahbutstill.creditcard.service.EncryptionService;
 import jakarta.persistence.*;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,14 +12,19 @@ public class CreditCardJPACallBack {
     @PreUpdate
     public void beforeInsertOrUpdateCallBack(CreditCard creditCard) {
         log.info("before update was called...");
+        creditCard.setCreditCardNumber(getEncryptionService().encrypt(creditCard.getCreditCardNumber()));
     }
-
 
     @PostPersist
     @PostLoad
     @PostUpdate
     public void postLoad(CreditCard creditCard) {
         log.info("postLoad was called...");
+        creditCard.setCreditCardNumber(getEncryptionService().decrypt(creditCard.getCreditCardNumber()));
+    }
+
+    private EncryptionService getEncryptionService() {
+        return SpringContextHelper.getApplicationContext().getBean(EncryptionService.class);
     }
 
 }
